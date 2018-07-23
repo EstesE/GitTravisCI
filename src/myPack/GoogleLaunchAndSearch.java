df313eb766cca25b199561100d60403649e2cd19
mithrilhall <eric_estes@aspensquare.com>
2018-07-23 10:10:47 -0400

    .

--- src/myPack/GoogleLaunchAndSearch.java
+++ src/myPack/GoogleLaunchAndSearch.java
@@ -1,31 +1,31 @@
-package myPack;
+// package myPack;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.firefox.FirefoxDriver;
+// import org.testng.annotations.Test;
 
-public class GoogleLaunchAndSearch {
+// public class GoogleLaunchAndSearch {
 
-	@Test
-	public void googleLaunch() throws Exception{
-		//Initiate Gecko driver
-		System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");
+// 	@Test
+// 	public void googleLaunch() throws Exception{
+// 		//Initiate Gecko driver
+// 		System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");
 		
-		WebDriver driver=new FirefoxDriver();
+// 		WebDriver driver=new FirefoxDriver();
 		
-		//Navigate to Google 
-		driver.get("https://google.com");
+// 		//Navigate to Google 
+// 		driver.get("https://google.com");
 		
-		//Maximize the window size
-		driver.manage().window().maximize();
+// 		//Maximize the window size
+// 		driver.manage().window().maximize();
 		
-		driver.findElement(By.id("lst-ib")).sendKeys("mobile wallpaper 1234567");
+// 		driver.findElement(By.id("lst-ib")).sendKeys("mobile wallpaper 1234567");
 		
-		driver.findElement(By.name("btnK")).click();
+// 		driver.findElement(By.name("btnK")).click();
 		
-		//Close the window
-		driver.close();
+// 		//Close the window
+// 		driver.close();
 		
-	}
-}
+// 	}
+// }
